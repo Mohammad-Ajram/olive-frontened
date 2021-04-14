00690a7f161811e955fbfe8ac44bd4ad47dbed27
Mohammad Ajram <mohammadajram5@gmail.com>
2021-04-14 18:43:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import "antd/dist/antd.css";
-import Topnav from "./components/nav/Topnav";
-import Footer from "./components/nav/Footer";
 import { Route, Switch } from "react-router-dom";
-import Home from "./pages/Home.js";
-import AdminLogin from "./pages/AdminLogin";
-import AddProperty from "./pages/admin/AddProperty";
-import UpdateProperty from "./pages/admin/UpdateProperty";
-import AllProperties from "./pages/admin/AllProperties";
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
 import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import RentSellProperty from "./pages/RentSellProperty";
-import FindProperty from "./pages/FindProperty";
-import PropertyDetails from "./pages/PropertyDetails";
-import ScrollToTop from "./components/ScrollToTop";
-import TopProperties from "./pages/TopProperties";
-import ResidentialProperties from "./pages/ResidentialProperties";
-import CommercialProperties from "./pages/CommercialProperties";
-import AgriculturalProperties from "./pages/AgriculturalProperties";
 import "./index.css";
-import NoMatch from "./pages/NoMatch";
+import { lazy, Suspense } from "react";
+const Topnav = lazy(() => import("./components/nav/Topnav"));
+const Footer = lazy(() => import("./components/nav/Footer"));
+
+const Home = lazy(() => import("./pages/Home.js"));
+const AdminLogin = lazy(() => import("./pages/AdminLogin"));
+const AddProperty = lazy(() => import("./pages/admin/AddProperty"));
+const UpdateProperty = lazy(() => import("./pages/admin/UpdateProperty"));
+const AllProperties = lazy(() => import("./pages/admin/AllProperties"));
+const AboutUs = lazy(() => import("./pages/AboutUs"));
+const ContactUs = lazy(() => import("./pages/ContactUs"));
+
+const RentSellProperty = lazy(() => import("./pages/RentSellProperty"));
+const FindProperty = lazy(() => import("./pages/FindProperty"));
+const PropertyDetails = lazy(() => import("./pages/PropertyDetails"));
+const ScrollToTop = lazy(() => import("./components/ScrollToTop"));
+const TopProperties = lazy(() => import("./pages/TopProperties"));
+const ResidentialProperties = lazy(() =>
+  import("./pages/ResidentialProperties")
+);
+const CommercialProperties = lazy(() => import("./pages/CommercialProperties"));
+const AgriculturalProperties = lazy(() =>
+  import("./pages/AgriculturalProperties")
+);
+
+const NoMatch = lazy(() => import("./pages/NoMatch"));
 
 function App() {
   const [token, setToken] = useState("");
@@ -31,62 +39,79 @@ function App() {
 
   return (
     <div className="App">
-      <ToastContainer />
-      <ScrollToTop />
-      <header className="App-header">
-        <Topnav token={token} setToken={setToken} />
-      </header>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route
-          exact
-          path="/admin-login"
-          render={(props) => <AdminLogin {...props} setToken={setToken} />}
-        />
+      <Suspense
+        fallback={
+          <div className="loading-container">
+            <div
+              className="loader"
+              style={{ fontSize: "48px", color: "rgb(2, 74, 54)" }}
+            >
+              <LoadingOutlined />
+            </div>
+          </div>
+        }
+      >
+        <ToastContainer />
+        <ScrollToTop />
+        <header className="App-header">
+          <Topnav token={token} setToken={setToken} />
+        </header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="/admin-login"
+            render={(props) => <AdminLogin {...props} setToken={setToken} />}
+          />
 
-        <Route
-          exact
-          path="/admin/add-property"
-          render={(props) => <AddProperty {...props} token={token} />}
-        />
-        <Route
-          exact
-          path="/admin/all-properties"
-          render={(props) => <AllProperties {...props} token={token} />}
-        />
-        <Route
-          exact
-          path="/admin/update-property/:id"
-          render={(props) => <UpdateProperty {...props} token={token} />}
-        />
-        <Route exact path="/about-us" component={AboutUs} />
-        <Route exact path="/contact-us" component={ContactUs} />
-        <Route
-          exact
-          path="/rent-or-sell-property"
-          component={RentSellProperty}
-        />
-        <Route exact path="/find-property" component={FindProperty} />
-        <Route exact path="/property-details/:id" component={PropertyDetails} />
-        <Route exact path="/top-properties" component={TopProperties} />
-        <Route
-          exact
-          path="/residential-properties"
-          component={ResidentialProperties}
-        />
-        <Route
-          exact
-          path="/commercial-properties"
-          component={CommercialProperties}
-        />
-        <Route
-          exact
-          path="/agricultural-properties"
-          component={AgriculturalProperties}
-        />
-        <Route component={NoMatch} />
-      </Switch>
-      <Footer />
+          <Route
+            exact
+            path="/admin/add-property"
+            render={(props) => <AddProperty {...props} token={token} />}
+          />
+          <Route
+            exact
+            path="/admin/all-properties"
+            render={(props) => <AllProperties {...props} token={token} />}
+          />
+          <Route
+            exact
+            path="/admin/update-property/:id"
+            render={(props) => <UpdateProperty {...props} token={token} />}
+          />
+          <Route exact path="/about-us" component={AboutUs} />
+          <Route exact path="/contact-us" component={ContactUs} />
+          <Route
+            exact
+            path="/rent-or-sell-property"
+            component={RentSellProperty}
+          />
+          <Route exact path="/find-property" component={FindProperty} />
+          <Route
+            exact
+            path="/property-details/:id"
+            component={PropertyDetails}
+          />
+          <Route exact path="/top-properties" component={TopProperties} />
+          <Route
+            exact
+            path="/residential-properties"
+            component={ResidentialProperties}
+          />
+          <Route
+            exact
+            path="/commercial-properties"
+            component={CommercialProperties}
+          />
+          <Route
+            exact
+            path="/agricultural-properties"
+            component={AgriculturalProperties}
+          />
+          <Route component={NoMatch} />
+        </Switch>
+        <Footer />
+      </Suspense>
     </div>
   );
 }
